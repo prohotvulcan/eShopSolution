cb20af7fe3076ac34c33190c809bd4a97f903462
Lê Ngân <prohotvulcan@outlook.com>
2020-01-27 12:36:02 +0700

    Add entity models

---
+++ eShopSolution.Data/EntityModels/CategoryTranslation.cs
@@ -0,0 +1,16 @@
+﻿namespace eShopSolution.Data.EntityModels
+{
+    public class CategoryTranslation
+    {
+        public int Id { get; set; }
+        public int CaterogyId { get; set; }
+        public string Name { get; set; }
+        public string SeoDescription { get; set; }
+        public string SeoTitle { get; set; }
+        public string LanguageId { get; set; }
+        public string SeoAlias { get; set; }
+
+        public Category Category { get; set; }
+        public Language Language { get; set; }
+    }
+}
